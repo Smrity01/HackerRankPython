5e9ee9b3b69b8aa8a0406351a20e2e7342864f74
Smrity chaudhary <smrity.mca17.du@gmail.com>
2017-10-28 21:37:03 +0530

    Documentation was missing...!!!

--- leapYear.py
+++ leapYear.py
@@ -1,4 +1,13 @@
 def checkLeap(year):
+	'''
+	Objective: To check whether the given year is leap year or not
+	Input parameter:
+			year: the user entered year
+	Return Value: Boolean value whether it is leap year or not
+	'''
+	#Approach: Check whether user entered year  is divisible by 4(not 100),and 400(and 100)
+	      #if yes then it is a leap year
+	      #if not then it is not a leap year
 	leap = False
 	if ((year%4)==0 and (year%100!=0)):
 		leap = True
@@ -9,6 +18,12 @@ def checkLeap(year):
 	return leap
 
 def main():
+	'''
+	Objective: To take input from user an call cheakLeap() function
+	Input parameter:none
+	Return Value: none
+	'''
+	
 	year = int(input('Enter an year : '))
 	leapYear = checkLeap(year)
 
